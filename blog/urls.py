6d7bb72cc54fe0b30d03fb5d72c4e0a499246712
Pauline Kelly <pauline.kelly@ericsson.com>
2018-02-07 17:12:28 +1100

    Create post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list') # only match on empty string
+    url(r'^$', views.post_list, name='post_list'), # only match on empty string
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
